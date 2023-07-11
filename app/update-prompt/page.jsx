27f5d792a00f128c1bc2005443346319633e6a74
Bilaal <73635058+bilaalukis@users.noreply.github.com>
2023-07-11 19:17:23 +0100

    API routes 2

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -5,27 +5,39 @@ import { useRouter, useSearchParams } from "next/navigation";
 
 import Form from "@components/Form";
 
-const EditPrompt = () => {
+const UpdatePrompt = () => {
   const router = useRouter();
-  const { data: session } = useSession();
   const searchParams = useSearchParams();
   const promptId = searchParams.get("id");
 
-  const [submitting, setIsSubmitting] = useState(false);
   const [post, setPost] = useState({ prompt: "", tag: "" });
+  const [submitting, setIsSubmitting] = useState(false);
 
-  useEffect(() => {}, [promptId]);
+  useEffect(() => {
+    const getPromptDetails = async () => {
+      const response = await fetch(`api/prompt/${promptId}`);
+      const data = await response.json();
 
-  const createPrompt = async (e) => {
+      setPost({
+        prompt: data.prompt,
+        tag: data.tag,
+      });
+    };
+
+    if (promptId) getPromptDetails();
+  }, [promptId]);
+
+  const updatePrompt = async (e) => {
     e.preventDefault();
     setIsSubmitting(true);
 
+    if (!promptId) return alert("Prompt ID not found");
+
     try {
-      const response = await fetch("/api/prompt/new", {
-        method: "POST",
+      const response = await fetch(`/api/prompt/${promptId}`, {
+        method: "PATCH",
         body: JSON.stringify({
           prompt: post.prompt,
-          userId: session?.user.id,
           tag: post.tag,
         }),
       });
@@ -42,13 +54,13 @@ const EditPrompt = () => {
 
   return (
     <Form
-      type="Create"
+      type="Update"
       post={post}
       setPost={setPost}
       submitting={submitting}
-      handleSubmit={createPrompt}
+      handleSubmit={updatePrompt}
     />
   );
 };
 
-export default EditPrompt;
+export default UpdatePrompt;
